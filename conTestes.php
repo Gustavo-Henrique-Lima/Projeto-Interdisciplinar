b9011264c3f7d793a894f22b696825adae9d0c9e
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-01 11:20:18 -0300

    Modificação na forma de conectar a base de dados

--- conTestes.php
+++ conTestes.php
@@ -1,3 +1,12 @@
 <?php
-    $banco = new PDO('mysql:host=localhost;dbname=projetointerdisciplinar', 'root','Mototaxi37264365')or print (mysql_error());
+    //$banco = new PDO('mysql:host=localhost;dbname=projetointerdisciplinar', 'root','Mototaxi37264365')or print (mysql_error());
+    $usuario="root";
+    $senha="Mototaxi37264365";
+    $database="projetointerdisciplinar";
+    $host="localhost";
+    $mysqli= new mysqli($host,$usuario,$senha,$database);
+    if($mysqli->error)
+    {
+        die("Fala ao conectar ao banco de dados".$mysqli->error);    
+    }
 ?>
