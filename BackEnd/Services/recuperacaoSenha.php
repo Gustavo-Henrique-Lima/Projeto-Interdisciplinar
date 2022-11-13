3936662f234b7324fa1057a91510223c9166d5ab
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-13 11:47:47 -0300

    Melhorias no controller de recuperação de senha, adição de criptografia hash e alerts de erros

--- BackEnd/Services/recuperacaoSenha.php
+++ BackEnd/Services/recuperacaoSenha.php
@@ -5,34 +5,79 @@
         $email=$mysqli->escape_string($email);
         if(!filter_var($email,FILTER_VALIDATE_EMAIL))
         {
-            $erro[]="Email inválido";
-            foreach($erro as $msg)
-            {
-                echo "$msg";
-            }
+            echo("
+                <script>
+                    const msg = 'E-mail inválido';
+                    const divMessage = document.querySelector('.alert');
+                    const message = document.createElement('p');
+                    message.classList.add('message');
+                    message.innerText = msg;
+                    divMessage.appendChild(message);
+                    setTimeout(() => 
+                    {
+                        message.style.display = 'none';
+                    }, 3000);
+            </script>");
         }
         else{
-            $sql="SELECT siepe FROM professor WHERE email='$email'";
+            $sql="SELECT nome FROM professor WHERE email='$email'";
             $execut=$mysqli->query($sql) or die($mysqli->error);
-            $dado=$execut->fetch_assoc();
+            $dado=$execut->fetch_array();
             $total=$execut->num_rows;
             if($total>0)
             {
-                $novaseha=md5(time());
-                if(mail($email,"Recuperação de senha","Sua nova senha é: ".$novaseha))
+                $senhaEmail=time();
+                $novasenha=password_hash($senhaEmail,PASSWORD_DEFAULT);
+                if(mail($email,"Redefinição de senha","Olá ".$dado["nome"]. ", sua senha foi alterada com sucesso, utilize a seguinte senha para acessar o MyFreq: ".$senhaEmail."\n Recomendamos que você acesse o MyFreq procure por redefinir senha e redefina a sua senha."))
                 {
-                    $sql_code="UPDATE professor SET senha='$novaseha' WHERE email='$email'";
+                    $sql_code="UPDATE professor SET senha='$novasenha' WHERE email='$email'";
                     $sql_query=$mysqli->query($sql_code) or die($mysqli->error);
-                    echo "Sucesso";
+                    echo("
+                    <script>
+                        const msg = 'Sucesso, por favor confira o seu e-mail!';
+                        const divMessage = document.querySelector('.alert');
+                        const message = document.createElement('p');
+                        message.classList.add('message');
+                        message.innerText = msg;
+                        divMessage.appendChild(message);
+                        setTimeout(() => 
+                        {
+                            message.style.display = 'none';
+                        }, 3000);
+                    </script>");
                 }
             }
             else{
-                $erro[]="Email não cadastrado";
-                foreach($erro as $msg)
-                {
-                    echo "$msg";
-                }
+                echo("
+                <script>
+                    const msg = 'E-mail não cadastrado';
+                    const divMessage = document.querySelector('.alert');
+                    const message = document.createElement('p');
+                    message.classList.add('message');
+                    message.innerText = msg;
+                    divMessage.appendChild(message);
+                    setTimeout(() => 
+                    {
+                        message.style.display = 'none';
+                    }, 3000);
+                </script>");
             }
         }
+    }
+    function campoVazio()
+    {
+        echo("
+            <script>
+                const msg = 'Preencha todos os campos';
+                const divMessage = document.querySelector('.alert');
+                const message = document.createElement('p');
+                message.classList.add('message');
+                message.innerText = msg;
+                divMessage.appendChild(message);
+                setTimeout(() => 
+                {
+                    message.style.display = 'none';
+                  }, 3000);
+            </script>");
     }
 ?>
