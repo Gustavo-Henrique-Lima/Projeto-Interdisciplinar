1ae1e76b5f497f5ad0a30d68a530ab3ecb12e588
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-16 19:28:31 -0300

    Criando sessão ainda sem passar o objeto, e colocando foto de perfil para os usuários

--- BackEnd/Services/loginDocente.php
+++ BackEnd/Services/loginDocente.php
@@ -2,7 +2,7 @@
     include_once("alertas.php");
     function efetuarLogin($siepe,$password)
     {
-        require_once('conexao.php');
+        include_once("conexao.php");
         $siepe=$mysqli->escape_string($siepe);
         $password=$mysqli->escape_string($password);
         $sql="SELECT * FROM professor WHERE siepe='$siepe'";
@@ -11,9 +11,17 @@
         $total=$execut->num_rows;
         if($total>0)
         {
+            include_once("../../BackEnd/Entities/Docente.php");
             if(password_verify($password,$dado["senha"]))
             {
-                echo "Bem-vindo ".$dado["nome"];
+                $professor=new Docente($dado["siepe"],$dado["nome"],$dado["sobrenome"],$dado["ultimonome"],$dado["email"],$dado["senha"],$dado["PATH"]);
+                if(!isset($_SESSION))
+                {
+                    session_start();
+                }
+                $_SESSION['nome']=$professor->getNome();
+                $_SESSION['foto']=$professor->getFoto();
+                header("Location: telaInicial.php");
             }
             else{
                 senhaInvalida();
