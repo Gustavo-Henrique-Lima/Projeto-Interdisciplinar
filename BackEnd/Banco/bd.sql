dc179678ae789f6976a1edc092fb4335c5f8f96c
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-16 11:10:56 -0300

    Mudanças na base de dados e criação da pasta arquivos para comportar a foto de perfil do usuário

--- BackEnd/Banco/bd.sql
+++ BackEnd/Banco/bd.sql
@@ -3,21 +3,23 @@ CREATE SCHEMA projetointerdisciplinar;
 CREATE TABLE professor
 (
 	siepe MEDIUMINT,
-    nome VARCHAR(80),
-    sobrenome VARCHAR(80),
-    ultimonome VARCHAR(80),
-    email VARCHAR(80),
-    senha VARCHAR(255),
+    nome VARCHAR(80) NOT NULL,
+    sobrenome VARCHAR(80) NOT NULL,
+    ultimonome VARCHAR(80) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    PATH VARCHAR(100),
 	PRIMARY KEY (siepe)
 );
 CREATE TABLE aluno
 (
 	matricula VARCHAR(20),
-    email VARCHAR(80),
-    nome VARCHAR(80),
-    sobrenome VARCHAR(80),
-    ultimonome VARCHAR(80),
-    senha VARCHAR(255),
+    email VARCHAR(80) NOT NULL,
+    nome VARCHAR(80) NOT NULL,
+    sobrenome VARCHAR(80) NOT NULL,
+    ultimonome VARCHAR(80) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    PATH VARCHAR(100),
 	PRIMARY KEY (matricula)
 );
 
@@ -25,13 +27,15 @@ CREATE TABLE estagio
 (
 	matricula_aluno VARCHAR(20),
     inicio TIMESTAMP,
-    fim TIMESTAMP,
-    descricao VARCHAR(80),
-    local VARCHAR(50),
+    fim TIMESTAMP NOT NULL,
+    descricao VARCHAR(80) NOT NULL,
+    local VARCHAR(50) NOT NULL,
     PRIMARY KEY (matricula_aluno,inicio),
 	FOREIGN KEY (matricula_aluno) REFERENCES aluno(matricula)
 );
-select * from professor;
+
+SELECT * FROM professor;
 SELECT * FROM aluno;
-INSERT INTO professor VALUES(001,'Gustavo Henrique','de Lima','Nascimento','ghln@discente.ifpe.edu.br','123');
-INSERT INTO aluno VALUES(001,'ens8@discente.ifpe.edu.br','Everson','Silva','Nascimento','123');
+SELECT * FROM estagio;
+INSERT INTO professor VALUES(001,'Gustavo Henrique','de Lima','Nascimento','ghln@discente.ifpe.edu.br','123',null);
+INSERT INTO aluno VALUES('001','ens8@discente.ifpe.edu.br','Everson','Silva','Nascimento','123',null);
