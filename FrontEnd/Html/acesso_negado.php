f374db5fb5f35e24b23571ec7e20747e68479702
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-21 18:31:28 -0300

    Criação das páginas acesso negado e calendário, faltando checar se existe sessão do usuário

---
+++ FrontEnd/Html/acesso_negado.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Acesso Negado!</title>
+    <!-- Botstrap css -->
+    <link
+      rel="stylesheet"
+      href="../node_modules/bootstrap/dist/css/bootstrap.css"
+    />
+    <!--Css Local-->
+    <link rel="stylesheet" href="../css/style.css" />
+    <!--Normalize Css-->
+    <link rel="stylesheet" href="../css/normalize.css" />
+  </head>
+  <body class="bgImage">
+    <section
+      style="width: 30rem"
+      class="text-center container mt-5 sombra pt-3 bg-white"
+    >
+      <div class="m-auto" style="width: 10rem">
+        <img width="100%" src="../images/Negado.gif" alt="" />
+      </div>
+      <span class="h5 text-danger"><strong>Acesso Negado!</strong></span>
+      <p class="text-primary pt-5">
+        Por favor, para acessar o sistema faça
+        login clicando no botão logo abaixo.
+      </p>
+      <a
+        href="../../index.php"
+        class="btn btnLogin btn-outline-primary btnSize mt-2 mb-4"
+      >
+        Página de Login
+      </a>
+    </section>
+  </body>
+</html>
