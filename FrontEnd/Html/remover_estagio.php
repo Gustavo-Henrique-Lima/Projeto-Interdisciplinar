0a0be887bf995dc3162bcb5d4b68d589cab369b8
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-27 15:57:47 -0300

    Criação das telas de CRUD do estágio

---
+++ FrontEnd/Html/remover_estagio.php
@@ -0,0 +1,156 @@
+<?php
+    include("../../BackEnd/Services/protect.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Botstrap css -->
+    <link
+      rel="stylesheet"
+      href="../node_modules/bootstrap/dist/css/bootstrap.css"
+    />
+    <!--Css Local-->
+    <link rel="stylesheet" href="../css/style.css" />
+    <!--Normalize Css-->
+    <link rel="stylesheet" href="../css/normalize.css" />
+    <title>Remover Estágio</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+      <a class="navbar-brand fontTextLeftHeader ms-1" href="criar_estagio.php"
+        >MyFreq</a
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="position-absolute top-1 end-0">
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a
+                class="nav-link active text-white"
+                aria-current="page"
+                href="criar_estagio.php"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="perfil_usuario_docente.php"
+                >Perfil</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Sair</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <section class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Meus estágios</h1>
+        </div>
+      </div>
+      <div class="row my-2 mx-1 py-2 border rounded">
+        <div class="col-12">
+          <form action="">
+            <div class="row">
+              <div>
+                <ul class="nav nav-pills nav-fill">
+                  <li class="nav-item">
+                    <a class="nav-link" href="criar_estagio.php"
+                      >Criar Estágio</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="atualizar_estagio.php"
+                      >Atualizar Estágio</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="pesquisar_estagio.php"
+                      >Pesquisar Estágio</a
+                    >
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="remover_estagio.php">Remover Estágio</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-12 mt-3">
+                <h2>Remover Estágio</h2>
+              </div>
+            </div>
+            <p class="text-center text-primary pt-3 pb-3">
+              Por favor, caso você necessite de informações para encontrar o estágio que deseja remover, acesse a aba "Pesquisar Estágio" e clique na opção "Visualizar todos os estágios" a fim de encontrar o estágio que você deseja remover.
+            </p>
+            <div class="row g-3">
+              <div class="col-12">
+                <label for="nomeDoAluno" class="form-label"
+                  >Nome do Aluno(a)</label
+                >
+                <input
+                  type="text"
+                  name="nomeDoAluno"
+                  id="nomeDoAluno"
+                  class="form-control"
+                />
+                <label for="numeroDaMatricula" class="form-label"
+                  >Número da Matrícula</label
+                >
+                <input
+                  type="text"
+                  name="numeroDaMatricula"
+                  id="numeroDaMatricula"
+                  class="form-control"
+                />
+                <label for="localEstagio" class="form-label"
+                  >Local do Estágio</label
+                >
+                <input
+                  type="text"
+                  name="localEstagio"
+                  id="localEstagio"
+                  class="form-control"
+                />
+              </div>
+              <div class="col-12">
+                <label for="dataEstagio" class="form-label"
+                  >Data e Hora do Estágio</label
+                >
+                <input
+                  type="datetime-local"
+                  name="dataEstágio"
+                  id="dataEstágio"
+                  class="form-control"
+                />
+              </div>
+              <div class="col-12">
+                <button type="submit" class="btn btn-danger w-100 text-white">
+                  <a href="" class="text-white">Remover Estágio</a>
+                </button>
+              </div>
+              
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+    <!--Adicionando Bootstrap Js-->
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <!--Script Local-->
+    <script src="../myScript/meuScript.js"></script>
+  </body>
+</html>
---
+++ FrontEnd/Html/retorno_pesq.php
@@ -0,0 +1,124 @@
+<?php
+    include("../../BackEnd/Services/protect.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Botstrap css -->
+    <link
+      rel="stylesheet"
+      href="../node_modules/bootstrap/dist/css/bootstrap.css"
+    />
+    <!--Css Local-->
+    <link rel="stylesheet" href="../css/style.css" />
+    <!--Normalize Css-->
+    <link rel="stylesheet" href="../css/normalize.css" />
+    <title>Pesquisar Estágio</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+      <a class="navbar-brand fontTextLeftHeader ms-1" href="criar_estagio.html"
+        >MyFreq</a
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="position-absolute top-1 end-0">
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a
+                class="nav-link active text-white"
+                aria-current="page"
+                href="criar_estagio.php"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="perfil_usuario.php"
+                >Perfil</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Sair</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <section class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Meus estágios</h1>
+        </div>
+      </div>
+      <div class="row my-2 mx-1 py-2 border rounded">
+        <div class="col-12">
+          <form action="">
+            <div class="row">
+              <div class="col-12 mt-3">
+                <h2>Informações do Estágio</h2>
+              </div>
+            </div>
+            <div class="row g-3 mt-4">
+              <div class="col-12">
+                <table class="table table-white">
+                  <thead>
+                    <tr>
+                      <th class="text-center" scope="col">#</th>
+                      <th class="text-center" scope="col">Nome do Aluno(a)</th>
+                      <th class="text-center" scope="col">
+                        Número da Matrícula
+                      </th>
+                      <th class="text-center" scope="col">Local do Estágio</th>
+                      <th class="text-center" scope="col">Data e Hora</th>
+                      <th class="text-center" scope="col">Descrição</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td class="text-center">João Da silva</td>
+                      <td class="text-center">2012ERBJ1239</td>
+                      <td class="text-center">
+                        Hospital Júlio Alves de Lira - Belo Jardim - PE
+                      </td>
+                      <td class="text-center">12/07/2022 20:00</td>
+                      <td class="text-center">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Iure minima sequi distinctio facere accusamus
+                        harum sint ullam voluptates voluptate voluptatibus.
+                        Dicta dolore inventore autem. Omnis ad quo hic dolorem
+                        assumenda!
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="col-12">
+                <a href="pesquisar_estagio.php" class="btn btn-primary w-100 text-white bg-danger"
+                  >Voltar</a
+                >
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+    <!--Adicionando Bootstrap Js-->
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <!--Script Local-->
+    <script src="../myScript/meuScript.js"></script>
+  </body>
+</html>
