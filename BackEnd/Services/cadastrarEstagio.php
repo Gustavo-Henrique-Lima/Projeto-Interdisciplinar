cfd29d0dfabb4d7334211c6136a8017609e8cf92
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-28 09:45:46 -0300

    Passando o valor de presença como nulo na criação do estágio

--- BackEnd/Services/cadastrarEstagio.php
+++ BackEnd/Services/cadastrarEstagio.php
@@ -6,7 +6,7 @@
         $matriculaAluno=$mysqli->escape_string($matriculaAluno);
         $descricao=$mysqli->escape_string($descricao);
         $local=$mysqli->escape_string($local);
-        $sql="INSERT INTO estagio (matricula_aluno,inicio,descricao,lugar) VALUES ('$matriculaAluno','$inicio','$descricao','$local')";
+        $sql="INSERT INTO estagio (matricula_aluno,inicio,descricao,lugar,presenca) VALUES ('$matriculaAluno','$inicio','$descricao','$local',null)";
         try{
             $execut=$mysqli->query($sql) or die($mysqli->error);
             cadastrouEstagio();
