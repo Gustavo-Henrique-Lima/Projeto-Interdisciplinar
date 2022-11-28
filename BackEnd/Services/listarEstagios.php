f4e49aa976ed74f4a153903dedf87e2333075c9d
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-28 16:34:58 -0300

    Criando consulta que lista todos os estágios

---
+++ BackEnd/Services/listarEstagios.php
@@ -0,0 +1,8 @@
+<?php
+        include_once("conexao.php");
+        $sql="SELECT estagio.lugar,estagio.descricao,estagio.inicio,aluno.nome,aluno.matricula
+        FROM estagio 
+        INNER JOIN aluno ON estagio.matricula_aluno=aluno.matricula
+        WHERE estagio.inicio>=now();";
+        $execut=$mysqli->query($sql) or die($mysqli->error);
+?>
