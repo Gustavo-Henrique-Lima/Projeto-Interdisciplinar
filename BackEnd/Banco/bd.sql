2cb55407501be45ed65a100372f73cfeb0c80e35
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-30 12:46:45 -0300

    Adicionando mais um usuário de testes

--- BackEnd/Banco/bd.sql
+++ BackEnd/Banco/bd.sql
@@ -29,16 +29,20 @@ CREATE TABLE estagio
     inicio TIMESTAMP,
     descricao VARCHAR(80) NOT NULL,
 	lugar VARCHAR(50) NOT NULL,
-    presenca VARCHAR(9),
+    presenca VARCHAR(22),
     PRIMARY KEY (matricula_aluno,inicio),
     FOREIGN KEY (matricula_aluno) REFERENCES aluno(matricula)
 );
 
 INSERT INTO professor VALUES(001,'Gustavo Henrique','de Lima','Nascimento','ghln@discente.ifpe.edu.br','123',null);
 INSERT INTO aluno VALUES('10','ens8@discente.ifpe.edu.br','Everson','Silva','Nascimento','123',null);
+INSERT INTO aluno VALUES('11','ecvs@discente.ifpe.edu.br','Edson','Cavalcante','Valença','123',null);
 
 SELECT * FROM professor;
 SELECT * FROM aluno;
 SELECT * FROM estagio;
 
 DELETE FROM estagio;
+
+UPDATE estagio SET presenca='Falta',lugar='Arrego' WHERE matricula_aluno='10';
+UPDATE estagio SET lugar='Atualizando',descricao='Atualizando',inicio='2027-12-09' WHERE matricula_aluno='$matricula' AND inicio='2022-11-28 10:51:00';
